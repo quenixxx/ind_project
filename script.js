1cbc80d4bbdc337e3fa1d077098d6fa964849ae2
quenixxx <topstorm@mail.ru>
2025-03-28 21:28:22 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function years(year){
+    if(years < 13){
+        console.log("drink toddy")
+    }
+    else if(years < 17){
+        console.log("drink coke")
+    }
+    else if(years < 21){
+        console.log("drink beer")
+    }
+    else if(years >= 21){
+        console.log("drink whisky")
+    }
+}
+years(17)
