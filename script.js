62fc89ff016dfd4a00752b7ad0588dfdd4e4d33d
quenixxx <topstorm@mail.ru>
2025-04-23 23:15:52 +0300

    add_time_animation

--- script.js
+++ script.js
@@ -47,3 +47,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('click', enableAudio);
 });
+function updateTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+
+    const timeText = `${hours}:${minutes}:${seconds}`;
+    document.querySelector(".time").textContent = timeText;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
